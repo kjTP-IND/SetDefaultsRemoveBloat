ac641e6b915eed4e7882a5aff35efa541cab5d52
kjTP-IND <144932028+kjTP-IND@users.noreply.github.com>
2023-10-17 15:23:52 -0400

    Update StartMe.bat

--- StartMe.bat
+++ StartMe.bat
@@ -1,1 +1,1 @@
-Powershell.exe -noprofile -NoExit -Command "&{Start-Process Powershell.exe -File '\"%~dp0DefaultAndRemoval.ps1\"' -Verb RunAs}"
+Powershell.exe -noprofile -ExecutionPolicy Bypass -NoExit -Command "&{Start-Process Powershell.exe -File '\"%~dp0DefaultAndRemoval.ps1\"' -Verb RunAs}"
